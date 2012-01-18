098b570825ffce73e9e8e504cb7da5c29e7e34cd
ccocchi <cocchi.c@gmail.com>
2012-01-18 19:47:54 +0100

    Change session cache store to be compliant with new rack version

--- lib/action_dispatch/session/libmemcached_store.rb
+++ lib/action_dispatch/session/libmemcached_store.rb
@@ -1,65 +1,69 @@
-begin
-  module ActionDispatch
-    module Session
-      class LibmemcachedStore < AbstractStore
-        require 'memcached'
+require 'memcached'
 
-        def initialize(app, options = {})
+module ActionDispatch
+  module Session
+    class LibmemcachedStore < AbstractStore
 
-          super
+      DEFAULT_OPTIONS = Abstract::ID::DEFAULT_OPTIONS.merge {
+        :prefix_key => 'rack:session',
+        :memcache_server => 'localhost:11211'
+      }
 
-          options[:expire_after] ||= options[:expires]
-          @default_options = {
-            :prefix_key => 'rack:session',
-            :memcache_server => 'localhost:11211'
-          }.merge(@default_options)
-
-          @pool = options[:cache] || Memcached.new(@default_options[:memcache_server], @default_options)
-          @mutex = Mutex.new
-
-          super
-        end
-
-        private
-        def get_session(env, sid)
-          sid ||= generate_sid
-          begin
-            session = @pool.get(sid) || {}
-          rescue Memcached::NotFound
-            session = {}
-          rescue Memcached::Error => e
-            log_error(e)
-            session = {}
-          end
-          [sid, session]
-        end
-
-        def set_session(env, sid, session_data)
-          options = env['rack.session.options']
-          expiry  = options[:expire_after] || 0
-          @pool.set(sid, session_data, expiry)
-          return sid
-        rescue Memcached::Error => e
-          log_error(e)
-          return false
-        end
-
-        def destroy(env)
-          if sid = current_session_id(env)
-            @pool.delete(sid)
-          end
-        rescue Memcached::Error => e
-          log_error(e)
-          return false
-        end
-
-        def log_error(exception)
-          logger ||= RAILS_DEFAULT_LOGGER
-          logger.error "MemcachedError (#{exception.inspect}): #{exception.message}" if logger && !@logger_off
-        end		  
+      def initialize(app, options = {})
+        options[:expire_after] ||= options[:expires]
+        super
+        @mutex = Mutex.new
+        @pool = options[:cache] || Memcached.new(@default_options[:memcache_server], @default_options)
       end
+
+      private
+
+      def generate_sid
+        loop do
+          sid = super
+          break sid unless @pool.get(sid, true)
+        end
+      end
+
+      def get_session(env, sid)
+        sid ||= generate_sid
+        session = with_lock(env, {}) do
+          begin
+            @pool.get(sid)
+          rescue Memcached::NotFound
+            {}
+          end
+        end
+        [sid, session]
+      end
+
+      def set_session(env, session_id, new_session, options)
+        expiry  = options[:expire_after]
+        expiry = expiry.nil? ? 0 : expiry + 1
+
+        with_lock(env, false) do
+          @pool.set(session_id, new_session, expiry)
+          session_id
+        end
+      end
+
+      def destroy_session(env, session_id, options)
+        with_lock(env, nil) do
+          @pool.delete(sid)
+          generate_sid unless options[:drop]
+        end
+      end
+
+      def with_lock(env, default)
+        @mutex.lock if env['rack.multithread']
+        yield
+      rescue Memcached::Error => e
+        logger.error e.error
+        default
+      ensure
+        @mutex.unlock if @mutex.locked?
+      end
+
     end
   end
-rescue LoadError
-  # MemCache wasn't available so neither can the store be
 end
